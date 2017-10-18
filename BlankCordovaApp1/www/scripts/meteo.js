012a2b103ee7d28d7a0759c943e1ab282c5e6972
antmag <maguet.antoine@hotmail.fr>
2017-10-18 13:40:17 +0200

    frist cpommit

---
+++ BlankCordovaApp1/www/scripts/meteo.js
@@ -0,0 +1,17 @@
+﻿
+
+function getMeteoByCp(CP){
+
+    console.log(CP);
+    var webMethod = "http://openweathermap.org/find?";
+    var webMethod = webMethod+"q="+CP;
+
+    $.ajax({
+        type: 'GET',
+        url: webMethod,
+        success: function (data) {
+            console.log(data);
+        }
+
+    });
+};
